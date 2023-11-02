8feb3289e8c586803a83c14f12fea5d629ae4278
KumpelStachu <stanislaw.rzeszut@gmail.com>
2023-11-02 18:12:35 +0100

    y

---
+++ galeria/login.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="pl">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Logowanie</title>
+  <link rel="shortcut icon" href="../images/favicon.ico" type="image/x-icon" />
+  <link rel="stylesheet" href="../styles/base.css" />
+  <script type="module" src="../scripts/year.js"></script>
+  <script type="module" src="../scripts/back.js"></script>
+  <script defer data-domain="php.code.kums.dev" src="https://a.kums.me/js/script.js"></script>
+</head>
+
+<body>
+  <header>
+    <a href="../index.html" data-back>Cofnij się do tyłu</a>
+  </header>
+
+  <main id="content">
+    <h1>Logowanie</h1>
+    <section class="card">
+
+    </section>
+  </main>
+
+  <footer id="footer">
+    <span>Lubię kotodziewczyny</span>
+    <span>
+      Copyright &copy; <span id="year">2022</span>&ensp;
+      <a href="https://github.com/KumpelStachu" target="_blank">Stanisław Rzeszut</a>
+    </span>
+  </footer>
+</body>
+
+</html>
